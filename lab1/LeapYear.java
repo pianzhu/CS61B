cdb464c612eb407b0b43418a040c42cc92a19b9c
klowe <pianzhu0x3f@gmail.com>
2022-09-14 11:45:41 +0800

    upload lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author zbj
  */
 public class LeapYear {
 
+	private static bool isLeapYear(int year){
+		if((year%4==0 && year%100!=0) || (year%400==0)){
+			return true;
+		}
+		return false;
+	}
+	
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
